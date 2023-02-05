d6aa287d5c1503076e320e5a4ee16f8e64581ce2
Kristina <zkvzkvzkv@yandex.ru>
2023-02-05 22:14:43 +0300

    создана программа по условию задачи 2

--- hw_9_task_2/Program.cs
+++ hw_9_task_2/Program.cs
@@ -1,2 +1,17 @@
-﻿// See https://aka.ms/new-console-template for more information
-Console.WriteLine("Hello, World!");
+﻿//  Задайте значения M и N. Напишите программу, 
+//  которая найдёт сумму натуральных элементов в промежутке от M до N.
+
+Console.WriteLine("Введите натуральное число M");
+int M = Convert.ToInt32(Console.ReadLine());
+
+Console.WriteLine("Введите натуральное число больше M");
+int N = Convert.ToInt32(Console.ReadLine());
+
+int sum = 0;
+int index = M;
+while (index <= N)
+{
+    sum = sum + index;
+    index++;
+}
+Console.WriteLine($"Сумма натуральных чисел в промежутке от {M} до {N} равна {sum}");
