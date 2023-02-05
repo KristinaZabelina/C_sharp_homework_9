ecfbe6a84009e8124a65535f0255adc705ef09df
Kristina <zkvzkvzkv@yandex.ru>
2023-02-05 22:10:22 +0300

    создана программа по условию задачи 1

---
+++ hw_9_task_1/Program.cs
@@ -0,0 +1,15 @@
+﻿// Задайте значение N. Напишите программу, которая выведет все натуральные 
+// числа в промежутке от N до 1. Выполнить с помощью рекурсии.
+
+Console.WriteLine("Введите число");
+int number =  Convert.ToInt32(Console.ReadLine());
+
+void NumberCounter(int number)
+{
+    if (number <= 0) return;
+    Console.WriteLine(number);
+    NumberCounter (number - 1);
+    
+}
+
+NumberCounter(number);
